1517aefa08d70e816a12e62bd2f7c39ba1e18025
Syohei YOSHIDA <syohex@gmail.com>
2013-11-06 11:11:15 +0900

    Fix for pep8

--- helm-pydoc.py
+++ helm-pydoc.py
@@ -7,7 +7,7 @@ import pkgutil
 
 myname = (splitext(basename(sys.argv[0])))[0]
 modules = [m[1] for m in pkgutil.iter_modules() if m[1] != myname]
-modules.append('sys') # builtin module
+modules.append('sys')  # builtin module
 modules.sort()
 
 for module in modules:
