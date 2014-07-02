4d6642f43830b6c5721d0d8fccedc7e88e5b9ee7
Syohei YOSHIDA <syohex@gmail.com>
2014-07-02 11:14:57 +0900

    Bump up version and update Changes

--- helm-pydoc.el
+++ helm-pydoc.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-pydoc
-;; Version: 0.05
+;; Version: 0.06
 ;; Package-Requires: ((helm "1.0") (cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
