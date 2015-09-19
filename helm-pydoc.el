30f1814b5b16db0413ffe74b0d0420b38e153df9
Syohei YOSHIDA <syohex@gmail.com>
2015-09-19 14:34:51 +0900

    Bump up version and update Changes

--- helm-pydoc.el
+++ helm-pydoc.el
@@ -1,10 +1,10 @@
 ;;; helm-pydoc.el --- pydoc with helm interface -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2014 by Syohei YOSHIDA
+;; Copyright (C) 2015 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-pydoc
-;; Version: 0.06
+;; Version: 0.07
 ;; Package-Requires: ((helm-core "1.7.4") (cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
