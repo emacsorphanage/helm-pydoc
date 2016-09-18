85480a29b56dacde425655bc8f5a597c785afdf5
Syohei YOSHIDA <syohex@gmail.com>
2016-09-18 14:42:25 +0900

    Update dependencies
    helm no longer supports Emacs 24.3.

--- helm-pydoc.el
+++ helm-pydoc.el
@@ -1,11 +1,11 @@
 ;;; helm-pydoc.el --- pydoc with helm interface -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2015 by Syohei YOSHIDA
+;; Copyright (C) 2016 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-pydoc
 ;; Version: 0.07
-;; Package-Requires: ((helm-core "1.7.4") (cl-lib "0.5"))
+;; Package-Requires: ((helm-core "2.0") (emacs "24.4"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -34,8 +34,7 @@
 
 (defcustom helm-pydoc-virtualenv "venv"
   "Directory name containing virtualenv."
-  :type 'string
-  :group 'helm-pydoc)
+  :type 'string)
 
 (defvar helm-pydoc--collect-command
   (if load-file-name
